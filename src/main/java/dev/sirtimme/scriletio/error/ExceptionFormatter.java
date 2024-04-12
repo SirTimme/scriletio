ce3e6e6ff32177e92a54fbd7b0e1fe388bcc7096
SirTimme <tim@sirtimme.dev>
2024-04-12 10:45:10 +0200

    include examples in error messages

--- src/main/java/dev/sirtimme/scriletio/error/ExceptionFormatter.java
+++ src/main/java/dev/sirtimme/scriletio/error/ExceptionFormatter.java
@@ -3,15 +3,21 @@ package dev.sirtimme.scriletio.error;
 public class ExceptionFormatter {
     public static String format(String content, ParsingException exception) {
         final var sb = new StringBuilder();
-        sb.append("Error: Incorrect use");
+        sb.append("**Error:**\n");
+        sb.append("Incorrect usage of the duration format. Error occurred here:");
         sb.append("\n```\n");
         sb.append(content);
         sb.append("\n");
         sb.repeat(" ", exception.getColumn());
         sb.append("^");
         sb.append("\n```\n");
-        sb.append("Cause: ");
+        sb.append("**Cause:**\n");
         sb.append(exception.getMessage());
+        sb.append("\n\n");
+        sb.append("**Examples:**\n");
+        sb.append("- `4D` or `4d` = 4 days\n");
+        sb.append("- `2D3H` or `2d3h` = 2 days 3 hours\n");
+        sb.append("- `1D5H3M` or `1d5h3m` = 1 day 5 hours 3 minutes");
         return sb.toString();
     }
 }
