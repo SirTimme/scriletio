e31539f500415d3f2d391cf2de0914668a6c17a9
SirTimme <tim@sirtimme.dev>
2024-04-21 10:38:59 +0200

    enable GUILD_MESSAGES intent

--- src/main/java/dev/sirtimme/scriletio/Main.java
+++ src/main/java/dev/sirtimme/scriletio/Main.java
@@ -3,10 +3,12 @@ package dev.sirtimme.scriletio;
 import dev.sirtimme.scriletio.events.EventHandler;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class Main {
     public static void main(String[] args) {
         JDABuilder.createLight(System.getenv("TOKEN"))
+                  .enableIntents(GatewayIntent.GUILD_MESSAGES)
                   .addEventListeners(new EventHandler())
                   .setActivity(Activity.playing("Silentium"))
                   .build();
