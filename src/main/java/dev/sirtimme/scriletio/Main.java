e04787877644c63f7adfe459c9eb39cea9f3cf41
SirTimme <tim@sirtimme.dev>
2024-04-23 13:03:31 +0200

    remove unneeded import

--- src/main/java/dev/sirtimme/scriletio/Main.java
+++ src/main/java/dev/sirtimme/scriletio/Main.java
@@ -3,7 +3,6 @@ package dev.sirtimme.scriletio;
 import dev.sirtimme.scriletio.events.EventHandler;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class Main {
     public static void main(String[] args) {
