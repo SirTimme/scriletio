f1b3884af70f58d63a72dab4eff4c643ca933b0b
SirTimme <tim@sirtimme.dev>
2024-05-14 18:50:53 +0200

    refactor dependencies to use toml file

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,13 +11,13 @@ repositories {
 }
 
 dependencies {
-	implementation("ch.qos.logback:logback-classic:1.5.3")
-	implementation("net.dv8tion:JDA:5.0.0-beta.20") {
+	implementation(libs.logback)
+	implementation(libs.jda) {
 		exclude(group = "club.minnced", module = "opus-java")
 	}
-	implementation("org.postgresql:postgresql:42.7.3")
-	implementation("org.hibernate:hibernate-core:6.4.4.Final")
-	implementation("org.hibernate:hibernate-hikaricp:6.4.4.Final")
+	implementation(libs.postgres)
+	implementation(libs.hibernate.core)
+	implementation(libs.hibernate.hikaricp)
 }
 
 tasks.jar {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,9 @@
 rootProject.name = "scriletio"
+
+dependencyResolutionManagement {
+	versionCatalogs {
+		create("libs") {
+			from(files("libs.versions.toml"))
+		}
+	}
+}
