d1cb017c0cc3618f540f3c05329fa694358b3ac6
SirTimme <tim@sirtimme.dev>
2024-07-13 10:12:40 +0200

    include open-telemetry to capture logs

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,15 @@ dependencies {
     implementation("org.postgresql:postgresql:42.7.3")
     implementation("org.hibernate:hibernate-core:6.4.4.Final")
     implementation("org.hibernate:hibernate-hikaricp:6.4.4.Final")
+
+    // OpenTelemetry
+    implementation(platform("io.opentelemetry:opentelemetry-bom-alpha:1.40.0-alpha"))
+    implementation(platform("io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom-alpha:2.5.0-alpha"))
+
+    implementation("io.opentelemetry:opentelemetry-sdk")
+    implementation("io.opentelemetry:opentelemetry-exporter-otlp")
+    implementation("io.opentelemetry.semconv:opentelemetry-semconv")
+    implementation("io.opentelemetry.instrumentation:opentelemetry-logback-appender-1.0")
 }
 
 tasks.jar {
