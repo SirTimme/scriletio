26217d1256f048a81dad445216327322767ddf69
SirTimme <tim@sirtimme.dev>
2024-11-16 10:32:21 +0100

    add quotes to logging messages

--- src/main/java/dev/sirtimme/scriletio/commands/event/GuildReadyCommand.java
+++ src/main/java/dev/sirtimme/scriletio/commands/event/GuildReadyCommand.java
@@ -24,28 +24,28 @@ public class GuildReadyCommand implements IEventCommand<GuildReadyEvent> {
     public void execute(final GuildReadyEvent event) {
         final var deleteConfigs = configRepository.findAll(event.getGuild().getIdLong());
 
-        LOGGER.debug("Found {} configs for guild with id {}", deleteConfigs.size(), event.getGuild().getIdLong());
+        LOGGER.debug("Found '{}' configs for guild with id '{}'", deleteConfigs.size(), event.getGuild().getIdLong());
 
         for (final var deleteConfig : deleteConfigs) {
             final var channel = event.getGuild().getChannelById(TextChannel.class, deleteConfig.getChannelId());
 
             if (channel == null) {
-                LOGGER.warn("Could not retrieve channel with id {}: Result was null", deleteConfig.getChannelId());
+                LOGGER.warn("Could not retrieve channel with id '{}': Result was null", deleteConfig.getChannelId());
                 continue;
             }
 
-            LOGGER.debug("Found {} delete tasks for channel with id {}", deleteConfig.getDeleteTasks().size(), deleteConfig.getChannelId());
+            LOGGER.debug("Found '{}' delete tasks for channel with id '{}'", deleteConfig.getDeleteTasks().size(), deleteConfig.getChannelId());
 
             for (final var iterator = deleteConfig.getDeleteTasks().iterator(); iterator.hasNext(); ) {
                 final var deleteTask = iterator.next();
 
                 try {
                     final var message = channel.retrieveMessageById(deleteTask.getMessageId()).complete();
-                    LOGGER.debug("Submitted delete task for message with id {}", deleteTask.getMessageId());
+                    LOGGER.debug("Submitted delete task for message with id '{}'", deleteTask.getMessageId());
 
                     deleteTaskManager.submitTask(deleteTask, message);
                 } catch (ErrorResponseException error) {
-                    LOGGER.warn("Could not retrieve message with id {}: {}", deleteTask.getMessageId(), error.getErrorResponse());
+                    LOGGER.warn("Could not retrieve message with id '{}': '{}'", deleteTask.getMessageId(), error.getErrorResponse());
 
                     iterator.remove();
                 }
