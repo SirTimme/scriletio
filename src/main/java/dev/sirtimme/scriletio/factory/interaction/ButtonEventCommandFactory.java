b573c3dc62b5163d9b672895a70a9367c767bb7d
SirTimme <tim@sirtimme.dev>
2024-11-16 10:58:55 +0100

    add localized responses for button interactions

--- src/main/java/dev/sirtimme/scriletio/factory/interaction/ButtonEventCommandFactory.java
+++ src/main/java/dev/sirtimme/scriletio/factory/interaction/ButtonEventCommandFactory.java
@@ -2,10 +2,10 @@ package dev.sirtimme.scriletio.factory.interaction;
 
 import dev.sirtimme.iuvo.api.commands.interaction.IInteractionCommand;
 import dev.sirtimme.iuvo.api.factory.interaction.IInteractionCommandFactory;
-import dev.sirtimme.scriletio.commands.interaction.component.button.AcceptDeletionButton;
-import dev.sirtimme.scriletio.commands.interaction.component.button.AcceptRegistrationButton;
-import dev.sirtimme.scriletio.commands.interaction.component.button.CancelDeletionButton;
-import dev.sirtimme.scriletio.commands.interaction.component.button.CancelRegistrationButton;
+import dev.sirtimme.scriletio.commands.interaction.component.button.DeleteAcceptButton;
+import dev.sirtimme.scriletio.commands.interaction.component.button.RegisterAcceptButton;
+import dev.sirtimme.scriletio.commands.interaction.component.button.DeleteCancelButton;
+import dev.sirtimme.scriletio.commands.interaction.component.button.RegisterCancelButton;
 import dev.sirtimme.scriletio.localization.LocalizationManager;
 import dev.sirtimme.scriletio.repository.DeleteConfigRepository;
 import dev.sirtimme.scriletio.repository.UserRepository;
@@ -21,10 +21,10 @@ public class ButtonEventCommandFactory implements IInteractionCommandFactory<But
     public ButtonEventCommandFactory(final LocalizationManager l10nManager) {
         this.buttonCommands = new HashMap<>();
 
-        registerButtonCommand("register-accept", context -> new AcceptRegistrationButton(new UserRepository(context), l10nManager));
-        registerButtonCommand("register-cancel", _ -> new CancelRegistrationButton(l10nManager));
-        registerButtonCommand("delete-accept", context -> new AcceptDeletionButton(new UserRepository(context), new DeleteConfigRepository(context), l10nManager));
-        registerButtonCommand("delete-cancel", _ -> new CancelDeletionButton(l10nManager));
+        registerButtonCommand("register-accept", context -> new RegisterAcceptButton(new UserRepository(context), l10nManager));
+        registerButtonCommand("register-cancel", _ -> new RegisterCancelButton(l10nManager));
+        registerButtonCommand("delete-accept", context -> new DeleteAcceptButton(new UserRepository(context), new DeleteConfigRepository(context), l10nManager));
+        registerButtonCommand("delete-cancel", _ -> new DeleteCancelButton(l10nManager));
     }
 
     @Override
